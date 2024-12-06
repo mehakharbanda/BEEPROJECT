63c9d07ee7264e038eddca07cf12a0d43eee0af0
mehakharbanda <122426669+mehakharbanda@users.noreply.github.com>
2024-12-06 16:10:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   ssl: true,  // This can be included if needed
-});
-
+})
 .then(() => {
   console.log("MongoDB connected successfully");
 })
